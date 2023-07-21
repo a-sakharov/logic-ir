6d03ab897b18aedbb5a63d4356d4a66f7b8a4a45
Aleksandr Sakharov <aleaksah@gmail.com>
2023-07-21 14:03:55 +0300

    File export added

--- source/LogicIRAnalyzerResults.cpp
+++ source/LogicIRAnalyzerResults.cpp
@@ -17,13 +17,10 @@ LogicIRAnalyzerResults::~LogicIRAnalyzerResults()
 
 void LogicIRAnalyzerResults::GenerateBubbleText(U64 frame_index, Channel &channel, DisplayBase display_base)
 {
+    Frame frame = GetFrame(frame_index);
     ClearResultStrings();
 
-    Frame frame = GetFrame(frame_index);
-    ClearTabularText();
-
     char number_str[128] = { 0 };
-
     if (frame.mFlags)
     {
         snprintf(number_str, sizeof(number_str), "Start");
@@ -42,38 +39,37 @@ void LogicIRAnalyzerResults::GenerateBubbleText(U64 frame_index, Channel &channe
 
 void LogicIRAnalyzerResults::GenerateExportFile(const char *file, DisplayBase display_base, U32 export_type_user_id)
 {
-    /*
     std::ofstream file_stream(file, std::ios::out);
 
-    U64 trigger_sample = mAnalyzer->GetTriggerSample();
-    U32 sample_rate = mAnalyzer->GetSampleRate();
-
-    file_stream << "Time [s],High,Low,Duty,Frequency" << std::endl;
-
     U64 num_frames = GetNumFrames();
-    for (U32 i = 0; i < num_frames; i++) {
+    for (U32 i = 0; i < num_frames; i++) 
+    {
         Frame frame = GetFrame(i);
 
-        char time_str[128];
-        AnalyzerHelpers::GetTimeString(frame.mStartingSampleInclusive, trigger_sample, sample_rate, time_str, 128);
+        char number_str[128] = { 0 };
+        if (frame.mFlags)
+        {
+            snprintf(number_str, sizeof(number_str), "\n[Start]");
+        }
+        else
+        {
+            U64 i;
+            for (i = 0; i < frame.mData2; ++i)
+            {
+                snprintf(number_str + strlen(number_str), sizeof(number_str) - strlen(number_str), "%llu", frame.mData1);
+            }
+        }
 
-        char number_str[128];
-        snprintf(number_str, sizeof(number_str), "%f,%f,%f,%.1f",
-                 Width(frame),
-                 mAnalyzer->Width(frame.mData1, frame.mEndingSampleInclusive),
-                 DutyCycle(frame),
-                 1000000.0 / mAnalyzer->Width(frame.mStartingSampleInclusive, frame.mEndingSampleInclusive));
+        file_stream << number_str;
 
-        file_stream << time_str << "," << number_str << std::endl;
-
-        if (UpdateExportProgressAndCheckForCancel(i, num_frames) == true) {
+        if (UpdateExportProgressAndCheckForCancel(i, num_frames) == true) 
+        {
             file_stream.close();
             return;
         }
     }
 
     file_stream.close();
-    */
 }
 
 void LogicIRAnalyzerResults::GenerateFrameTabularText(U64 frame_index, DisplayBase display_base)
@@ -82,6 +78,7 @@ void LogicIRAnalyzerResults::GenerateFrameTabularText(U64 frame_index, DisplayBa
     ClearTabularText();
 
     char number_str[128] = {0};
+    
     if (frame.mFlags)
     {
         snprintf(number_str, sizeof(number_str), "\n[Start]");
@@ -94,8 +91,12 @@ void LogicIRAnalyzerResults::GenerateFrameTabularText(U64 frame_index, DisplayBa
             snprintf(number_str + strlen(number_str), sizeof(number_str) - strlen(number_str), "%llu", frame.mData1);
         }
     }
-    
+
+#ifdef NDEBUG
     AddTabularText(number_str);
+#else
+    AddTabularText(number_str, "-");
+#endif
 }
 
 void LogicIRAnalyzerResults::GeneratePacketTabularText(U64 packet_id, DisplayBase display_base)
